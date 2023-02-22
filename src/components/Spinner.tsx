42cb263b8b424d5dceed62e3711c3b3c002c0bd6
rafhael josino <rafhael_josino1@hotmail.com>
2023-02-21 23:32:55 -0300

    Spinner component can receive a message to display above its spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,14 @@
 import { Circles } from "react-loader-spinner";
 
-function Spinner() {
+type Props = {
+    message: string,
+}
+
+function Spinner(props: Props) {
+    const {message} = props;
+
     return <div className="divSpinner">
+        {message === '' ? null : <div>{message}</div>}
         <Circles
             height="80"
             width="80"
